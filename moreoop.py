c0b79c860c28f8bb434c4970ca7be4362581a2aa
fatimamhsn <fatima.mohsin@laetottenham.ac.uk>
2024-11-11 12:26:45 +0000

    final

---
+++ moreoop.py
@@ -0,0 +1,37 @@
+class employee:
+
+    def __init__(self, name, role,salary, dob):
+        self.name = name
+        self.role = role
+        self.salary = salary
+        self.dob = dob
+
+    def increase_salary(self):
+        self.salary = self.salary * 1.1
+
+    def calculate_age(self):
+        year = int(dob[6,10])
+        age = 2024 - year
+        return age
+
+
+class manager:
+
+    def __init__(self, name, role, salary, dob, bonus)
+        self.name = name
+        self.role = role
+        self.salary = salary 
+        self.dob = dob
+        self.bonus = bonus
+
+    def increase_salary(self):
+        self.salary  = self.slary * 1.2
+    
+    def calculate_age(self):
+        year = int(self.dob[6:10])
+        age = 2024 - year
+        return age  
+      
+    def increase_bonus(self):
+        bonus = bonus * 1.05
+    
